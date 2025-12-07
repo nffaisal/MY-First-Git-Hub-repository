2a6eb0e1edafe7cc1de16b10fe7d9b2557e1ee4b
nffaisal <noorffatima0045@gmail.com>
2025-12-07 23:07:36 +0500

    Initial commit of C++ project files

--- labTasks.cpp
+++ labTasks.cpp
@@ -1,29 +1,44 @@
 #include<iostream>
 #include<cmath>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 double getaverageofdigits(int number);
 int cubeOfDigits(int number);
-void isarmstrong(int sum, int number);
+bool isarmstrong(int sum, int number);
 void displayevendigits(int number);
 bool iseven(int number);
  double futureInvestmentValue( double investmentAmount, double monthlyInterestRate, int years);
 void displayinvestmentrate(double investmentAmount, double monthlyInterestRate, int years);
-double millimeterstoinches(double millimeters);
-double inchestomillimeters(double inches);
-void  millimetertoinchetable(int millimeters);
 double Sumseries(int n);
 void tablesumseries(int n);
+void numberOfDaysInFebruary(int startyear, int endyear);
+void printMatrix(int n);
+int reversenumber(int number);
+bool ispalindrome(int number);
+int diceroll();
+void craps();
 int main(){
       //cout<<getaverageofdigits(2468);
-      //isarmstrong(cubeOfDigits(153), 153);
+      int number;
+        /*if(isarmstrong(cubeOfDigits(153), 153)){
+               number = reversenumber(153);
+               if(isarmstrong(cubeOfDigits(number) ,number)){
+                cout<<"reverse and armstrong";
+               }else{
+                cout<<"is noy revrse armstrong";
+               }
+        } */
       // displayevendigits(24567);
       //double investmentrate, amount;
       //cout<<"enter inevstemnet rate"; cin>>investmentrate;
       //cout<<" enter investment amount ";cin>>amount;
       //displayinvestmentrate(amount, investmentrate, 30);
       // millimetertoinchetable(10);
-      tablesumseries(4);
-
+        //numberOfDaysInFebruary(2000,2004);
+        // printMatrix(4);
+        //cout<<ispalindrome(121);
+        //craps();
   return 0;         
 }
 double getaverageofdigits(int number){
@@ -51,11 +66,11 @@ int cubeOfDigits(int number){
      return sumofdigits;
 }
 
-void isarmstrong(int sum,int num){
+bool isarmstrong(int sum,int num){
    if(sum == num){
-    cout<<"It is armstrong number";
+    return true;
    }else{
-     cout<<"It is not";
+     return false;
    }
 }
 void displayevendigits(int number){
@@ -77,27 +92,72 @@ double futureInvestmentValue( double investmentAmount, double monthlyInterestRat
    int futureinvestment = investmentAmount * pow((1 + monthlyInterestRate/100), years);
    return futureinvestment;
 }
-void displayinvestmentrate(double investmentAmount, double monthlyInterestRate, int years){
-  cout<<"Year    FutureInvestment";
-  for(int i =1;i<years; i++ ){
-    cout<<i<<" "<<futureInvestmentValue(investmentAmount, monthlyInterestRate, i)<<endl;
-  }
-}
-void  millimetertoinchetable(int millimeters){
-   cout<<"Millimeters  Inches ||   Inches Millimeters"<<endl;
-   for(int i =0; i<millimeters;i++){
-    cout<<i<<" "<<millimeterstoinches(i)<<"  "<<i<<"  "<<inchestomillimeters(i)<<endl;
-   }
-}
-double Sumseries(int n){
-   if(n>=0){
-    return Sumseries(n-1) + 1.0/(n* ( n +2.0 ));
-}else{
-  return 0;
-}}
 
-void tablesumseries(int n){
-  for(int i =1; i<n;i++){
-    cout<<i<<" "<<Sumseries(i);
+double Sumseries(int n){
+   if(n>0){
+    return Sumseries(n-1) + 1.0/(n* ( n +2.0 )); } else{
+   return 0;}
+}
+void numberOfDaysInFebruary(int startyear, int endyear){
+  for(int i = startyear ;i<endyear;i++)
+  if((i%4== 0 && i %100 != 0) || i%400==0){
+    cout<<"29"<<endl;
+  }else{
+    cout<<"28"<<endl;
   }
+}
+ void printMatrix(int n){
+       srand(time(0));
+     for(int i =0; i<n;i++){
+      for(int j =0;j<n;j++){
+         cout<<rand() %2<<" ";
+      }
+      cout<<endl;
+     }
+ }
+ int reversenumber(int number){
+     int digit;
+     const int original = number;
+     int reversed =0;
+     while(number>0){
+       digit = number % 10;
+         int reversed =(reversed *10 ) + digit;
+         number = number /10;
+     }
+     return reversed;
+ }
+bool ispalindrome(int number){
+   if(reversenumber(number) == number){ return true;}
+   else{ return false;}
+}
+int diceroll(){
+  srand(time(0));
+  return ( rand() % 6) +1;
+}
+void craps(){
+     int sum;
+      int gameover =false;
+       int point =0;
+    
+    int first =diceroll(); int second =diceroll();
+    cout<<"\n Your First Roll:  "<<first;
+    cout<<"\n Your second Roll: "<<second;
+     sum = first +second;
+    while(!gameover){
+      switch(sum){
+        case 4:
+        case 5:
+        case 6:
+        case 8:
+        case 9:
+        case 10: 
+               cout<<"\n Point "<<sum; break;
+       case 2:
+       case 3:
+       case 12:
+       cout<<"\nYou lose"; gameover =true ; break;
+       case 7:
+       case 11:
+       cout<<"\n You WIN!"; break;}
+      }
 }
