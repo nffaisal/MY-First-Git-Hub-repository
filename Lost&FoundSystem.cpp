dc9bc2744f305066c233ed08c240e653714f46e8
nffaisal <noorffatima0045@gmail.com>
2025-12-13 13:46:56 +0500

    new update

--- Lost&FoundSystem.cpp
+++ Lost&FoundSystem.cpp
@@ -12,9 +12,11 @@ int counter_lost_items =0;
 std::vector<std::string> Lost_Items;
 std::vector<std::string> lost_phones;
 std::vector<std::string> lost_book;
-std::vector<std::string> miscellanious;
+std::vector<std::string> miscellanious_items;
 std::vector<std::string> date_lost;
 std::vector<std::string> lost_headphones;
+std::vector<std::string> location_lost;
+std::vector<int> person_to_contact;
 
 std::vector<int> Student_id_card;
  int check_student_Id(std::string Student_ID){
@@ -58,7 +60,7 @@ std::vector<int> Student_id_card;
             cin>>book_name; lost_book.push_back(book_name);
             case 7:
             cout<<"Enter Miscellanious Item name\n"; counter_lost_items++;
-            cin>>miscellanious; miscellanious.push_back(miscellanious);
+            cin>>miscellanious; miscellanious_items.push_back(miscellanious);
             default:
             cout<<"Exit \n";
             return;
@@ -74,12 +76,33 @@ std::vector<int> Student_id_card;
     cin>>year;
     date = date +"\\ " +weekday+"\\" + year;
     date_lost.push_back(date) ;}
+void person_Contact(){
+    std::string person_contact;
+    
+    cout<<"Enter phone number: ";
+    do{
+    cin>>person_contact;
+    if( person_contact.length()!= 11){
+        cout<<"Phone Number must be 11 digits: Re-Enter: \t";}
+    else{ break;}
+    }while(true);
+    person_to_contact.push_back(std::stoi(person_contact));
+
+}
+void location_Lost(){
+    std::string location;
+    cout<<"Enter location of findin lost item:\n";
+    cin>>location;
+    location_lost.push_back(location);
+}
  int main(){
     int option;
     cout<<"LOST AND FOUND MANAGEMENT SYSTEM"<<endl;
     cout<<"Menu:"<<endl;
-    cout<<"1. Store lost Items:the time of losing it,date of losing it";
-    cout<<"2. Update lost Item to found:";
+    cout<<"1. Register lost Items:the time of losing it,date of losing it, place of finding lost item";
+    cout<<"\n2. Search for lost Item:";
+    cout<<"\n 3.Display Count Of Lost Items: ";
+    
     do{
         cin>>option;
         if(option>2 || option <1){
@@ -89,7 +112,11 @@ std::vector<int> Student_id_card;
     if(option==1){
         categorize_items();
         date_missing();
+        person_Contact();
     }else if(option ==2){
+      cout<<"choose object lost: ";
+      cout<<"1. Phone \n 2. Wallet \n 3.Headphones \n 4. Debit Card\n 5.Student ID card \n6. Book \n7. Miscellanious\n ";\
+      switch()
 
     }
 
